268d806b6391c3ecf7b9a0b9543d1a489cde066e
Juhana S <juhana.suhonen@codemate.com>
2023-06-04 20:18:44 +0300

    Add models folder

---
+++ models/animals.go
@@ -0,0 +1,13 @@
+package models
+
+// GetArrIdx returns the idx of the object in the given slice (array)
+// Returns an error if item is not found
+// Todo: Uncomment once Dog struct exists
+// func (item Dog) GetArrIdx(slice []Dog) (int, error) {
+// 	for idx, v := range slice {
+// 		if v.Name == item.Name {
+// 			return idx, nil
+// 		}
+// 	}
+// 	return 0, fmt.Errorf("no index found for item %v in slice %v", item, slice)
+// }
