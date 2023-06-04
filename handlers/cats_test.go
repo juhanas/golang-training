d5b944ac6d3734360dc4252af74acd683da45f06
Juhana S <juhana.suhonen@codemate.com>
2023-06-04 16:20:21 +0300

    Add testcase for cat not found

--- handlers/cats_test.go
+++ handlers/cats_test.go
@@ -58,3 +58,29 @@ func TestGetCat(t *testing.T) {
 			rr.Body.String(), expected)
 	}
 }
+
+func TestGetCatNotFound(t *testing.T) {
+	req, err := http.NewRequest("GET", "/cat/?name=not-found", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	// Use external httprouter package's handler to allow path params
+	router := httprouter.New()
+	router.GET("/cat/", GetCatHandler)
+
+	router.ServeHTTP(rr, req)
+
+	wantedStatus := http.StatusBadRequest
+	if status := rr.Code; status != wantedStatus {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, wantedStatus)
+	}
+
+	expected := "could not find cat with name 'not-found'"
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
