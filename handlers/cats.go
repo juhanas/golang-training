a9eafd1ff32838d8d39c7e5bd44318f3256ee4fd
Juhana S <juhana.suhonen@codemate.com>
2023-06-04 15:33:34 +0300

    Add error handling for PostCatHandler

--- handlers/cats.go
+++ handlers/cats.go
@@ -57,11 +57,18 @@ func GetCatHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params)
 
 // findNameKey returns the key for the given name
 // Returns error if name is reserved or key could not be found
-func findNameKey(name string, dict map[string]string, i int) string {
+func findNameKey(name string, dict map[string]string, i int) (string, error) {
+	if i > len(name) {
+		return "", fmt.Errorf("index out of bounds")
+	}
+	if i == 0 {
+		return "", fmt.Errorf("index must be greater than 0")
+	}
+
 	key := name[:i]
 	_, ok := dict[key]
 	if !ok {
-		return key
+		return key, nil
 	}
 	return findNameKey(name, dict, i+1)
 }
@@ -69,9 +76,20 @@ func findNameKey(name string, dict map[string]string, i int) string {
 // PostCatHandler adds a new cat
 func PostCatHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	r.ParseForm()
-	name := r.Form["name"][0]
-	key := findNameKey(name, cats, 1)
-	cats[key] = name
+	if r.Form == nil || r.Form["name"] == nil || len(r.Form["name"]) < 1 || r.Form["name"][0] == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		io.WriteString(w, "no name for cat given")
+		return
+	}
 
+	name := r.Form["name"][0]
+	key, err := findNameKey(name, cats, 1)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		io.WriteString(w, err.Error())
+		return
+	}
+
+	cats[key] = name
 	w.WriteHeader(http.StatusOK)
 }
