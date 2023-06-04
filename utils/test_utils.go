c762e2cbe6368c96f1abcd932c6cf7d4ee4fd2eb
Juhana S <juhana.suhonen@codemate.com>
2023-06-04 20:36:30 +0300

    Add test utils in preparation for structs

--- utils/test_utils.go
+++ utils/test_utils.go
@@ -1,5 +1,8 @@
 package utils
 
+// Todo: Uncomment when dog struct is available
+// import "github.com/juhanas/golang-training/models"
+
 // CompareLists returns if the two lists contain the same data
 func CompareLists(list1, list2 []string) bool {
 	if list1 == nil && list2 != nil || list1 != nil && list2 == nil {
@@ -24,3 +27,38 @@ func CopyMap(orig map[string]string) map[string]string {
 	}
 	return new
 }
+
+// CompareDogs returns if the two lists of dogs contain the same data
+// Todo: Uncomment when dog struct is available
+// func CompareDogs(list1, list2 []models.Dog) bool {
+// 	if list1 == nil && list2 != nil || list1 != nil && list2 == nil {
+// 		return false
+// 	}
+// 	if len(list1) != len(list2) {
+// 		return false
+// 	}
+// 	for i := 0; i < len(list1); i++ {
+// 		if list1[i].Name != list2[i].Name {
+// 			return false
+// 		}
+// 		if list1[i].Color != list2[i].Color {
+// 			return false
+// 		}
+// 		if list1[i].Pack != list2[i].Pack {
+// 			return false
+// 		}
+// 	}
+// 	return true
+// }
+
+// CreateDog creates a Dog struct with the given name and other placeholder values
+// Todo: Uncomment when dog struct is available
+// func CreateDog(name string) models.Dog {
+// 	return models.Dog{
+// 		Animal: models.Animal{
+// 			Name:  name,
+// 			Color: "black",
+// 		},
+// 		Pack: "Who let the dogs out?",
+// 	}
+// }
